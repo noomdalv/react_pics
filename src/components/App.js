a28f085da012bde232d2e4b6e10ee41cb36c9df1
noomdalv <vladlunasan@gmail.com>
2020-06-09 00:00:10 -0500

    added axios api default config file

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	onSearchSubmit(term) {
-		axios.get("https://api.unsplash.com/search/photos", {
-			params: {	query: term	},
-			headers: {
-				Authorization: "Client-ID kVhg076qkBQTm2-mfVW5eCDEDqjBsyZLNfKZpqMLZtI"
-			}
+	state = {
+		images: []
+	}
+
+	onSearchSubmit = async (term) => {
+		const response = await unsplash.get("/search/photos", {
+			params: {	query: term	}
 		})
+		this.setState({ images: response.data.results })
 	}
 
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: 10 }}>
 				<SearchBar onSubmit={this.onSearchSubmit} />
+				{ `Found ${this.state.images.length} images`}
 			</div>
 		)
 	}
