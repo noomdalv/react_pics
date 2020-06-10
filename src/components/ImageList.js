bb841697a18fe4d6aa759970e66a88bbf300da46
noomdalv <vladlunasan@gmail.com>
2020-06-10 01:12:05 -0500

    added css styling and created ImageCard component to render image grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-	const images = props.images.map(({ description, id, urls }) => {
-		return <img src={urls.regular} alt={description} key={id} />
+	const images = props.images.map(img => {
+		return <ImageCard key={img.id} image={img} />
 	})
 
 	return (
-		<div>
+		<div className="image-list">
 			{ images }
 		</div>
 	)
